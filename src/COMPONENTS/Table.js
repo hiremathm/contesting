e77a17396fb75143249ecb2957dca7025fd4e0e4
Shivakumara M <shivasorab@gmail.com>
2021-01-17 22:27:07 +0530

    modal added

--- src/COMPONENTS/Table.js
+++ src/COMPONENTS/Table.js
@@ -1,26 +1,6 @@
 import React, {Component} from 'react'
 import DataTable from 'react-data-table-component';
 
-const customStyles = {
-  rows: {
-    style: {
-      minHeight: '72px', // override the row height
-    }
-  },
-  headCells: {
-    style: {
-      paddingLeft: '8px', // override the cell padding for head cells
-      paddingRight: '8px',
-    },
-  },
-  cells: {
-    style: {
-      paddingLeft: '8px', // override the cell padding for data cells
-      paddingRight: '8px',
-    },
-  },
-};
-
 const columns = [
   {
     name: 'Title',
@@ -30,26 +10,22 @@ const columns = [
   {
     name: 'Game Id',
     selector: 'game_id',
-    sortable: true,
-    right: true,
+    sortable: true
   },
   {
     name: 'No of Winners',
     selector: 'winners',
-    sortable: true,
-    right: true,
+    sortable: true
   },
   {
     name: 'Status',
     selector: 'status',
-    sortable: true,
-    right: true,
+    sortable: true
   },
   {
   	name: 'Winners Declaration Date',
   	selector: 'date',
-    sortable: true,
-    right: true,
+    sortable: true
   }
 ];
 
@@ -59,17 +35,9 @@ class MyComponent extends Component {
   render() {
     return (
       <DataTable
-        title="All Contests"
         columns={columns}
         data={this.props.data}
-        
-        highlightOnHover
-        pointerOnHover
-        responsive
         pagination
-        fixedHeader
-        subHeaderAlign='left'
-	    customStyles={customStyles}
       />
     )
   }
--- src/CONTAINERS/Contests.js
+++ src/CONTAINERS/Contests.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { connect } from 'react-redux'
-import Table from 'react-bootstrap/Table'
+import {Table, Modal} from 'react-bootstrap'
 import {Col, Row, Container} from 'react-bootstrap'
+import {NavLink} from 'react-router-dom'
 
 import Card from '../UI/Card'
-
-import classes from '../CSS/Contests.module.css'
+import Button from '../UI/Button'
 
 // Actions
 import {getContests} from '../REDUX_STORE/ACTIONS/ContestAction'
 
 // components
 import MyComponent from '../COMPONENTS/Table'
-class Contests extends React.Component {
 
+import tableClasses from '../CSS/Table.module.css'
+import classes from '../CSS/Contests.module.css'
+
+class Contests extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {show: false}
+	}
 	componentDidMount = () => {
 		this.props.dispatch(getContests())
+	}
+
+	setShow = (value) => {
+		this.setState({show: value})
 	}
 
 	render (){
@@ -30,7 +41,32 @@ class Contests extends React.Component {
 			<Card cardstyles = {classes.Contests}>
 				<Row lg={12} md={12} sm={12} xs={12}>
 					<Col lg={12} md={12} sm={12} xs = {12}>
-						{/*<Table>
+						<Button size = "small" onClick = {() => this.setShow(true)}>+ Add Contest</Button>
+						<Modal
+					        show={this.state.show}
+					        onHide={() => this.setShow(false)}
+					        dialogClassName="modal-90w"
+					        aria-labelledby="example-custom-modal-styling-title"
+					      >
+					        <Modal.Header closeButton>
+					          <Modal.Title id="example-custom-modal-styling-title">
+					            Custom Modal Styling
+					          </Modal.Title>
+					        </Modal.Header>
+					        <Modal.Body>
+					          <p>
+					            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+					            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+					            ipsam atque a dolores quisquam quisquam adipisci possimus
+					            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+					            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+					            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+					            deleniti rem!
+					          </p>
+					        </Modal.Body>
+					    </Modal>
+
+						{<Table className = {tableClasses.Table}>
 						  <thead>
 						    <tr>
 						      <th>#</th>
@@ -50,14 +86,15 @@ class Contests extends React.Component {
 										<td>{contest.contest_unique_id}</td>
 										<td>{contest.no_of_winners}</td>
 										<td>{contest.status}</td>
-										<td>{contest.winners_declared_at}</td>
+										<td>{new Date(contest.winners_declared_at).toDateString()}</td>
 										<td>actions</td>
 							    	</tr>
 							  	
 							  )}
 						  </tbody>
-						</Table>*/}
-						{(this.props.contests.contests.length > 0) && <MyComponent data = {data}/>}
+						</Table>}
+
+						{/*(this.props.contests.contests.length > 0) && <MyComponent data = {data}/>*/}
 					</Col>
 				</Row>
 			</Card>
