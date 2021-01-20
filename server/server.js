1363a9a945b1ee077868b514e8e1ebe83d8adadf
Shivakumara M <shivasorab@gmail.com>
2021-01-20 22:38:07 +0530

    server config changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT || 6060;
+const port = process.env.PORT || 3001;
 
 const publicPath = path.join(__dirname, '..',  'build')
 console.log("PUBLIPATH", publicPath)
