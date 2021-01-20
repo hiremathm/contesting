07e3026ad33d1a55e6dda4a9bdebaa3f4c2a505f
Shivakumara M <shivasorab@gmail.com>
2021-01-20 22:18:23 +0530

    folder structure changed

--- server/server.js
+++ server/server.js
@@ -4,8 +4,7 @@ const app = express()
 
 const port = process.env.PORT || 6060;
 
-
-const publicPath = path.join(__dirname,'..',  'frontend/build')
+const publicPath = path.join(__dirname, '..',  'build')
 console.log("PUBLIPATH", publicPath)
 app.use(express.static(publicPath));
 
