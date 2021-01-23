a6d8a08868e2f2d3d349d1e4a97a39175e5b1b93
Shivakumara M <shivasorab@gmail.com>
2021-01-23 19:26:03 +0530

    page size logic changes

--- src/CONTAINERS/Contests/Contests.js
+++ src/CONTAINERS/Contests/Contests.js
@@ -17,6 +17,9 @@ class Contests extends React.Component {
 	state = {
 		show: false,
 		no_of_records: 5,
+		from: 0,
+		end: 5,
+		page: 1,
 		override: css`
 			display: block;
 			text-align: center;
@@ -48,11 +51,32 @@ class Contests extends React.Component {
 		if(no_of_records === "#"){
 			no_of_records = 5
 		}
-		this.setState({no_of_records})
+		this.setState({
+			no_of_records: parseInt(no_of_records),
+			page: 1,
+			from: 0,
+			end: parseInt(no_of_records)
+		})
+	}
+
+	changePageSize = (value) => {
+		if(value === "increment"){
+			this.setState(prevState => ({
+				page: prevState.page + 1,
+				from: prevState.end,
+				end: prevState.end + prevState.no_of_records
+			}))
+		}else{
+			this.setState(prevState => ({
+				page: prevState.page - 1,
+				from: prevState.from - prevState.no_of_records,
+				end: prevState.end - prevState.no_of_records
+			}))
+		}
 	}
 
 	render (){
-		// console.log("COMPONENT RENDERED")
+		console.log("STATE", this.state)
 		return (
 		<Container>
 			<Card cardstyles = {classes.Contests}>
@@ -69,7 +93,7 @@ class Contests extends React.Component {
 						<Table 
 							edit = {this.editContest}
 							remove = {this.removeContest} 
-							rows = {this.props.contests.contests.slice(0,this.state.no_of_records)} 
+							rows = {this.props.contests.contests.slice(this.state.from,this.state.end)} 
 							cols = {["#","Title","Game ID","Winners","Status","Declaration Date","Actions"]}
 						/>
 						<div className={classes.PaginationStyle}>
@@ -79,8 +103,8 @@ class Contests extends React.Component {
 								<option value ="10">10</option>
 								<option value ="15">15</option>
 							</select>
-							<Button size = "small" >Previous</Button>
-							<Button size = "small" >Next</Button>
+							<Button size = "small" onClick = {() => this.changePageSize("decrement")} disabled={this.state.page === 1}>Previous</Button>
+							<Button size = "small" onClick = {() => this.changePageSize("increment")} disabled={Math.ceil((this.props.contests.contests.length/this.state.no_of_records)) === this.state.page}>Next</Button>
 						</div>
 					</Col>
 					</Row>
